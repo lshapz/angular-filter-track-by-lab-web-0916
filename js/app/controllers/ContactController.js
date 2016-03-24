fddf182e7ee68716aa1ddaaf8ae92e50132adcc5
Todd Motto <todd@toddmotto.com>
2016-03-24 02:25:15 +0000

    chore: Added solution

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,4 +1,4 @@
-function ContactController() {
+function ContactController($filter) {
 	this.contacts = [{
 		name: 'Bill Gates',
 		email: 'bill@microsoft.com',
@@ -20,6 +20,14 @@ function ContactController() {
 		phone: '75934988239',
 		username: 'obamaY0'
 	}];
+
+	this.search = '';
+
+	this.refilter = function () {
+		this.filteredList = $filter('filter')(this.contacts, this.search);
+	};
+
+	this.refilter();
 }
 
 angular
